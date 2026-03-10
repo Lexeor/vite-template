96361c40df6903a89e939031494d2311c3f61f53
Alexander Tarasov <lexeor.ross@gmail.com>
2026-03-10 15:07:14 +0100

    update styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { OverlayScrollbars } from 'overlayscrollbars';
+import 'overlayscrollbars/overlayscrollbars.css';
+import './index.css';
+import App from './App.tsx';
+
+OverlayScrollbars(document.body, {
+  scrollbars: {
+    autoHide: 'scroll',
+    autoHideDelay: 600,
+    theme: 'os-theme-dark',
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
